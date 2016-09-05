66386a8d18d09d696c96a629926c501e2712bdec
Alexis 'Horgix' Chotard <alexis.horgix.chotard@gmail.com>
2016-09-05 11:33:17 +0200

    Put Docker Hub credentials from pass to env

--- ec2_auth.sh
+++ ec2_auth.sh
@@ -2,3 +2,5 @@
 
 export AWS_ACCESS_KEY_ID=`pass xebia/aws/ansible-key-id`
 export AWS_SECRET_ACCESS_KEY=`pass xebia/aws/ansible-key-secret`
+export DOCKER_HUB_USERNAME=`pass dockerhub/username`
+export DOCKER_HUB_PASSWORD=`pass dockerhub/password`
